543723bcc6ac63daa3a9d03376ed1def5800493f
Reed Slaby <slabyr@uwplatt.edu>
2013-05-08 14:07:41 +0200

    Added More LCD Code

--- PWM/PWM.cpp
+++ PWM/PWM.cpp
@@ -19,14 +19,29 @@
  *
 \*---------------------------------------------------------------------*/
 
+//Definitions
 #define F_CPU 8000000
+
+//Includes
 #include <avr/interrupt.h>
 #include <avr/io.h>
 #include <util/delay.h>
 
-void LCDInit();
+//Function Prototypes
+void LCDChar(char text, unsigned char cursorPosition);
 void LCDCommand(unsigned char command);
 void LCDData(unsigned char data);
+void LCDInit();
+void LCDString(char * text, unsigned char cursorPosition);
+void LCDVoltage(float number, unsigned char cursorStartPos);
+
+//Global Variables
+volatile uint8_t theLowADC;
+volatile uint16_t theTenBitResults;
+volatile float setVoltage;
+
+//Constants
+const int charOffset = 3;
 
 int main(void)
 {
@@ -43,26 +58,28 @@ int main(void)
 	//Arbitrarily Set OCR1A (Duty Cycle)
 	OCR1A = 0x05;
 	//Enable INT0 and INT1
-	GICR = 1<<INT0 | 1<<INT1;
+	GICR |= 1<<INT0 | 1<<INT1;
 	//Set Falling Edge Trigger for Interrupts
-	MCUCR = 1<<ISC01 | 1<<ISC00 | 1<<ISC11 | 1<<ISC10;
-	//set ADC prescaler to division of 16, so at a clk f of 8Mhz, ADC speed is 500kHz
-	ADCSRA |= 1<<ADPS2; //see table 85 in datasheet for prescaler selection options
-	//set voltage reference as AVCC, should be 5volts?  see page 208
-	//we should maybe change this to a more stable reference?
+	MCUCR |= 1<<ISC01 | 1<<ISC00 | 1<<ISC11 | 1<<ISC10;
+	//Set ADC prescaler to division of 16, so at a clk f of 8Mhz,
+	//ADC speed is 500kHz. See table 85 in datasheet for prescaler 
+	//selection options.
+	ADCSRA |= 1<<ADPS2; 
+	//Set voltage reference as AVCC, should be 5 V?  See Page 208
 	ADMUX |= 1<<REFS0;
-	//enable ADC interrupt
+	//Enable ADC interrupt
 	ADCSRA |= 1<<ADIE;
-	//enable the ADC
+	//Enable the ADC
 	ADCSRA |= 1<<ADEN;
 	//Re-enable Interrupts
 	sei();
 	
 	LCDInit();
-	
+	LCDString("SET:   RTV: ",0x80);
+	LCDString("00.00  00.00",0xC0);
     while(1)
     {
-		
+		LCDVoltage(setVoltage,0xC0);
     }
 }
 
@@ -86,40 +103,95 @@ ISR(INT1_vect){
 
 }
 
-ISR(ADC_vect) //ADC interrupt vector function
+//This interrupt reads the ADC port and converts it to a usable value.
+//Params: in
+ISR(ADC_vect)
 {
-	uint8_t theLowADC = ADCL;  //assign the variable theLowADC as the value in the register ADCL
-	uint16_t theTenBitResults = ADCH<<8;  //assign the variable theTenBitResults as the value in ADCH shifted 8 left.
-	//Display the value on the Display
-	ADCSRA |=1<<ADSC;  //start ADC conversion
+	//Assign the variable theLowADC as the value in the register ADCL
+	theLowADC = ADCL; 
+	//Assign theTenBitResults as the value in ADCH shifted 8 left. 
+	theTenBitResults = ADCH<<8;  
+	//Start ADC conversion
+	ADCSRA |=1<<ADSC;  
 }
 
-void LCDInit()
+//This function sends a Character to the specified cursor position on 
+//the LCD Display.
+//Params: in, in
+void LCDChar(char text, unsigned char cursorPosition)
 {
-	_delay_ms(32);
-	LCDCommand(0x38);
-	LCDCommand(0x0C);
-	LCDCommand(0x01);
-	_delay_ms(1);
-	LCDCommand(0x04);
+	LCDCommand(cursorPosition);
+	LCDData(text);
 }
 
+//This function sends a Register command to the LCD Display.
+//Params: in
 void LCDCommand(unsigned char command)
 {
 	PORTB = command;
-	PORTD |= 1<<PIND7;
 	PORTD &= ~0x03;
-	_delay_ms(1);
-	PORTD &= ~1<<PIND7;
-	_delay_ms(1);
-}
-
-void LCDData(unsigned char data)
-{
-	PORTB = data;
-	PORTD |= (1<<PIND7 | 1<<PIND1);
-	PORTD &= ~0x01;
+	PORTD |= 0x80;
 	_delay_ms(1);
 	PORTD &= ~0x80;
 	_delay_ms(1);
+}
+
+//This function sends a character to the display.
+//Params: in
+void LCDData(unsigned char data)
+{
+	PORTB = data;
+	PORTD &= ~0x01;
+	PORTD |= 0x82;	
+	_delay_ms(1);
+	PORTD &= ~0x83;
+}
+
+//This function initializes the LCD Display.
+//Params: none
+void LCDInit()
+{
+	_delay_ms(32);
+	//Function Set
+	LCDCommand(0x38);
+	//Display On
+	LCDCommand(0x0E);
+	//Clear Display
+	LCDCommand(0x01);
+	_delay_ms(1);
+	//Set Entry Mode
+	LCDCommand(0x80);
+}
+
+//This function sends a string value to the specified cursor position on 
+//the LCD display.
+//Params: ref, in
+void LCDString(char* text, unsigned char cursorPosition)
+{
+	LCDCommand(cursorPosition);
+	while(*text)
+	{
+		LCDData(*text);
+		text++;
+	}
+}
+
+//This function converts a float Voltage to characters and sends them to 
+//the specified cursor position on the LCD Display.
+//Params: in, in
+void LCDVoltage(float number, unsigned char cursorStartPos)
+{
+	char tens = ((int)number - ((int)number % 10))/10;
+	char ones = (int)(number - tens);
+	char tenth = (int)((number - (int)number)*10);
+	char hundredth = (int)((((number - (int)number)*10)-tenth)*10 + 0.5);
+	tens += charOffset;
+	ones += charOffset;
+	tenth += charOffset;
+	hundredth += charOffset;
+	
+	LCDChar(tens,cursorStartPos);
+	LCDChar(ones,cursorStartPos + 1);
+	LCDChar(tenth,cursorStartPos + 3);
+	LCDChar(hundredth,cursorStartPos + 4);
 }
