aa4dda33f74309d46d2b28265cd28e69007594a8
Reed Slaby <slabyr@uwplatt.edu>
2013-06-10 15:46:50 +0200

    And Then

--- PWM/PWM.cpp
+++ PWM/PWM.cpp
@@ -10,7 +10,6 @@
  *
  *	Input:   PIND2 - Voltage Up
  *			 PIND3 - Voltage Down
- *			 PINA0 - ADC input
  *
  *	Output:  PIND5 - PWM
  *			 PORTB - LCD Data Bus
@@ -22,14 +21,6 @@
 
 //Definitions
 #define F_CPU 8000000
-
-// Define various Gains for PID. initial hit and miss....
-#define P_GAIN 0.5  //initial 0.8
-#define I_GAIN 0.001 //initial 0.005
-#define D_GAIN 0.000  //initial 0.01
-#define _dt_ 0.5
-#define mult 1000
-#define maxvoltage 30 //set this based on the voltage divider setup used (max measurable voltage)
 
 //Includes
 #include <avr/interrupt.h>
@@ -48,30 +39,22 @@ void LCDVoltage(float number, unsigned char cursorStartPos);
 volatile uint8_t theLowADC;
 volatile uint16_t theTenBitResults;
 volatile float setVoltage;
-int set_voltage = 1*maxvoltage/1024, previous_error = 0;
-int error, feedback_voltage, output;
-int D_error = 0, I_error = 0;
-
 
 //Constants
-const int charOffset = 3;
+const int charOffset = 0x30;
 
 int main(void)
 {
 
 	//Disable Interrupts
 	cli();
-	//Set data direction
 	DDRD = 0xA3;
 	DDRB = 0xFF;
-	DDRA |= (1 << 1);
-	// set PORTA initial values
-	PORTA = 0x00;
 	//Set counter options
 	TCCR1A = 0xA2;
 	TCCR1B = 0x19;
 	//Set TOP = ICR1 for 24.5kHz
-	ICR1 = 0x090;
+	ICR1 = 0x14D;
 	//Arbitrarily Set OCR1A (Duty Cycle)
 	OCR1A = 0x05;
 	//Enable INT0 and INT1
@@ -82,54 +65,21 @@ int main(void)
 	//ADC speed is 500kHz. See table 85 in datasheet for prescaler 
 	//selection options.
 	ADCSRA |= 1<<ADPS2; 
-	//Set voltage reference as AVCC, should be 5 V? binary= 01000000  See Page 208 or214
-	ADMUX = 0x40;
-	//Enable Start Conversion
-	ADCSRA |= 1<<ADSC;
+	//Set voltage reference as AVCC, should be 5 V?  See Page 208
+	ADMUX |= 1<<REFS0;
 	//Enable ADC interrupt
 	ADCSRA |= 1<<ADIE;
 	//Enable the ADC
 	ADCSRA |= 1<<ADEN;
-	
 	//Re-enable Interrupts
 	sei();
 	
-	//LCDInit();
-	//LCDString("SET:   RTV: ",0x80);
-	//LCDString("00.00  00.00",0xC0);
-    
-	//setup PID 
-	previous_error = set_voltage - feedback_voltage; //calculate the previous difference between set and ADC input voltage
-	
-	
-	while(1)
+	LCDInit();
+	LCDString("  ADC Voltage:  ",0x80);
+	LCDString("     00.00V     ",0xC0);
+    while(1)
     {
-		//_delay_ms(4000*_dt_); //delay within the loop
-		
-		
-		//Calculate Proportional Error
-		error = set_voltage - feedback_voltage;	
-		
-	    //Calculate Integral Error by summing up small small errors
-	    I_error += (error)*_dt_;
-		   
-		//Calculate Differential Error, by dividing error by time interval
-		D_error = (error - previous_error)/_dt_;
-
-		//Get output by summing up respective errors multiplied with their respective Gains
-		output = (P_GAIN * error) + (I_GAIN * I_error) + (D_GAIN * D_error);
-		
-		//set OCR1A Duty cycle to output
-		OCR1A = output;
-		
-		//Update Previous error
-		previous_error = error;
-			  
-		PORTB = 0x00; //led display
-		
-		//_delay_ms(20); //debug delay
-		
-		//LCDVoltage(setVoltage*5,0xC0);
+		//LCDVoltage(setVoltage,0xC0);
     }
 }
 
@@ -137,34 +87,20 @@ int main(void)
 //cycle.
 //Params: in
 ISR(INT0_vect){
-	//raise set point voltage if less than the max... 
-	if(setVoltage < maxvoltage)   
-		{
-			setVoltage += maxvoltage/1024;
-			set_voltage = setVoltage*mult;  //update integer variable of set voltage
-		}
-		
-		 
-	/////////////////////// This loop was to test, changing the duty cycle... for the actual project, we need to adjust the set voltage
-	// if(OCR1A <= (ICR1 - 1)) 
-		//OCR1A += 0x01;
-	//////////////////////
+	if(OCR1A <= (ICR1 - 1))
+
+		OCR1A += 0x01;
+
 }
 
 //This interrupt takes an input from a button and decreases the duty
 //cycle.
 //Params: in
 ISR(INT1_vect){  
-	//lower set point voltage
-	if(setVoltage > 0)
-	{
-		setVoltage -= maxvoltage/1024;
-		set_voltage = setVoltage*mult;  //update integer variable of set voltage
-	}
-	////////////////////// This loop was to test, changing the duty cycle... for the actual project, we need to adjust the setvoltage
-	//if(OCR1A > 0x01)
-		//OCR1A -= 0x01;
-	////////////////////////
+	if(OCR1A > 0x01)
+
+		OCR1A -= 0x01;
+
 }
 
 //This interrupt reads the ADC port and converts it to a usable value.
@@ -176,8 +112,7 @@ ISR(ADC_vect)
 	//Assign theTenBitResults as the value in ADCH shifted 8 left. 
 	theTenBitResults = ADCH<<8;  
 	//Start ADC conversion
-	ADCSRA |=1<<ADSC;
-	
+	ADCSRA |=1<<ADSC;  
 }
 
 //This function sends a Character to the specified cursor position on 
@@ -186,6 +121,7 @@ ISR(ADC_vect)
 void LCDChar(char text, unsigned char cursorPosition)
 {
 	LCDCommand(cursorPosition);
+	_delay_ms(1);
 	LCDData(text);
 }
 
@@ -206,10 +142,10 @@ void LCDCommand(unsigned char command)
 void LCDData(unsigned char data)
 {
 	PORTB = data;
-	PORTD &= ~0x01;
-	PORTD |= 0x82;	
+	PORTD &= ~0x02;
+	PORTD |= 0x81;	
 	_delay_ms(1);
-	PORTD &= ~0x83;
+	PORTD &= ~0x81;
 }
 
 //This function initializes the LCD Display.
@@ -218,14 +154,14 @@ void LCDInit()
 {
 	_delay_ms(32);
 	//Function Set
-	LCDCommand(0x38);
+	LCDCommand(0x3C);
 	//Display On
-	LCDCommand(0x0E);
+	LCDCommand(0x0F);
 	//Clear Display
 	LCDCommand(0x01);
 	_delay_ms(1);
 	//Set Entry Mode
-	LCDCommand(0x80);
+	LCDCommand(0x06);
 }
 
 //This function sends a string value to the specified cursor position on 
