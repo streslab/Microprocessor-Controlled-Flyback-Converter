538d0b18e707e07e23a1f4f0338239e09cd9b0bd
Reed Slaby <slabyr@uwplatt.edu>
2013-07-05 11:46:54 +0200

    Final Demo Code

--- PWM/PWM.cpp
+++ PWM/PWM.cpp
@@ -6,54 +6,38 @@
  *
  *	Purpose: This program is designed to regulate and control a flyback
  *			 converter.  The output voltage should be held at a user-
- *			 defined value while the input voltage and the load varies 
- *			 randomly.
+ *			 defined value while the input voltage varies randomly.
  *
- *  Design Parameters:
- *			  Input Voltage:  24V - 36V
- *			 Output Voltage:   0V - 24V
- *			 Output Current: < 2A
- *
- *	Input:   PINA0 - Feedback Input
- *			 PIND2 - Voltage Up
+ *	Input:   PIND2 - Voltage Up
  *			 PIND3 - Voltage Down
  *
- *	Output:  PORTB - LCD Data Bus
+ *	Output:  PIND5 - PWM
+ *			 PORTB - LCD Data Bus
  *			 PIND0 - LCD Register Select
  *			 PIND1 - LCD Read/Write
- *			 PIND5 - PWM
  *			 PIND7 - LCD Enable
  *
 \*---------------------------------------------------------------------*/
 
-//Define Constants
-#define ADC_FACTOR		2.96
-#define CHAR_OFFSET		0x30
-#define F_CPU			8000000
-#define I_COUNT_CYCLES	488
-#define I_LOWER_LIMIT  -500
-#define I_UPPER_LIMIT	500
-#define MAX_VOLTAGE		3000 //3000 = 30.00 volts
-#define ADC_TO_D		14
+//Definitions
+#define F_CPU 8000000
 
-#define SET_VOLTAGE_INCREMENT	50
-#define SWITCH_DEBOUNCE_CYCLES	100
-#define SWITCH_DEBOUNCE_MAX		1000
-
+// Define various Gains for PID. initial hit and miss....
 #define P_GAIN	4  //initial 0.8
 #define I_GAIN	2.5 //initial 0.05
 #define D_GAIN	0  //initial 0.01
-#define T		2048 //time per pid sample (us)
+#define T		2048
+ 
+
 
 //Includes
-#include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdlib.h>
+#include <avr/io.h>
 #include <tgmath.h>
 #include <util/delay.h>
+#include <stdlib.h>
 
 //Function Prototypes
-void InitializeuC();
 void LCDChar(char text, unsigned char cursorPosition);
 void LCDCommand(unsigned char command);
 void LCDData(unsigned char data);
@@ -62,141 +46,167 @@ void LCDString(char * text, unsigned char cursorPosition);
 void LCDVoltage(int number, unsigned char cursorStartPos);
 
 //Global Variables
-volatile uint16_t tenbitadc;
-volatile int previouserror = 0, ierror = 0;
-volatile unsigned int switchdebounce = 0, setvoltage = 100, icounter = 0;
+volatile uint8_t theLowADC;
+volatile uint16_t theTenBitResults;
+volatile int set_voltage = 0;  //set_voltage is an integer value (ex. 150 = 1.50 volts)
+volatile int previous_error = 0;  //for the PID
+volatile int I_error = 0;
+volatile int switchdebounce = 0;
+volatile int feedback_voltage;
+
+//Constants
+const int charOffset = 0x30;
+const int maxvoltage = 2400; //3000 = 30.00 volts
+const int setvoltageincrement = 50;
 
 int main(void)
 {
-	//Initialize the Microcontroller
-	InitializeuC();
-	//Set up LCD
+	//Disable Interrupts
+	cli();
+	//Set data direction
+	DDRD = 0xA3;
+	DDRC = 0x00;
+	DDRB = 0xFF;
+	DDRA = 0x00;
+	// set PORTA initial values
+	PORTA = 0x00;
+	//Set counter options
+	TCCR0 = 0x03;
+	TCCR1A = 0xA2;
+	TCCR1B = 0x19;
+	//Enable Timer0 Overflow Interrupt
+	TIMSK |= 0x01;
+	//Set TOP = ICR1 for 24.5kHz 
+	ICR1 = 0x090;
+	//Arbitrarily Set OCR1A (Duty Cycle)
+	OCR1A = 0x05;
+	//Enable INT0 and INT1
+	GICR |= 1<<INT0 | 1<<INT1;
+	//Set Falling Edge Trigger for Interrupts
+	MCUCR |= 1<<ISC01 | 1<<ISC00 | 1<<ISC11 | 1<<ISC10;
+	//Set ADC prescaler to division of 16, so at a clk f of 8Mhz,
+	//ADC speed is 500kHz. See table 85 in datasheet for prescaler
+	//selection options.
+	ADCSRA |= 1<<ADPS2;
+	//Set voltage reference as AVCC, should be 5 V? binary= 01000000  See Page 208 or214
+	ADMUX = 0x40;
+	//Enable Start Conversion
+	ADCSRA |= 1<<ADSC;
+	//Enable ADC interrupt
+	ADCSRA |= 1<<ADIE;
+	//Enable the ADC
+	ADCSRA |= 1<<ADEN;
 	LCDInit();
 	LCDString(" ADC       SET  ",0x80);
 	LCDString("00.00V    00.00V",0xC0);
-	
-	//Set up PID Initial Value
-	previouserror = setvoltage - tenbitadc;
-
+	//Re-enable Interrupts
+	sei();
+	//setup PID
+	previous_error = set_voltage - feedback_voltage; //calculate the previous difference between set and ADC input voltage
+	//int corrected_adc = feedback_voltage * 3;
 	while(1)
 	{
-		LCDVoltage(tenbitadc * ADC_FACTOR,0xC0);
-		LCDVoltage(abs(ierror) * ADC_FACTOR,0x85);
-		LCDVoltage(setvoltage,0xCA);
+		LCDVoltage(feedback_voltage*2.96,0xC0);  //the ADC Voltage
+		//LCDVoltage(abs(I_error)*2.96,0x85);
+		LCDVoltage(set_voltage,0xCA);
 		_delay_ms(200);
 	}	
 }
 
-//-----------------------------------------------------------------------
-//This interrupt at the Timer0 Overflow.  Runs the PID Code
-//Params: in
-//-----------------------------------------------------------------------
+//This interrupt at the timer0 frequency runs the PID code
+//Params:
 ISR(TIMER0_OVF_vect) 
 {
 	//Assign local variables
-	int error, pidoutput, derror = 0;
-	
+	int error,pidoutput;
+	int D_error = 0;
 	//Increment Switch Debouncer
-	if(switchdebounce <= SWITCH_DEBOUNCE_MAX)
+	if(switchdebounce <= 1000)
 		switchdebounce++;
-	//Wipe the integral error every second	
-	if(icounter >= I_COUNT_CYCLES)
-		ierror = 0;
 		
-	//Calculate Errors
-	error = setvoltage/ADC_FACTOR - tenbitadc;
-	ierror += (error*T/10000)* I_GAIN;
-	derror = (error - previouserror);
+	feedback_voltage = theTenBitResults;
+	//Calculate Proportional Error
+	error = set_voltage/2.96 - feedback_voltage;
 	
-	//Keep I within predefined limits
-	if(ierror >= I_UPPER_LIMIT)
-		ierror = I_UPPER_LIMIT;
-	else if(ierror <= I_LOWER_LIMIT)
-		ierror = I_LOWER_LIMIT;
-
-	//Get output by summing up errors multiplied with their Gains
-	pidoutput = ((((P_GAIN*error)+(ierror)+(D_GAIN*derror))+tenbitadc)/ADC_TO_D)+.5;
+	if(set_voltage > 0)
+	{
+		//Calculate Integral Error by summing up small small errors
+		I_error += (error*T/10000)* I_GAIN;
 		
-	OCR1A = pidoutput;	
-	previouserror = error;
-	icounter++;
+		if(I_error >= 500)
+			I_error -= 100;
+		else if(I_error <= -400)
+			I_error += 100;
+	
+		//Calculate Differential Error, by dividing error by time interval
+		D_error = (error - previous_error);
+
+		//Get output by summing up respective errors multiplied with their respective Gains
+		pidoutput = (P_GAIN * error) + (I_error) + (D_GAIN * D_error);
+	
+		pidoutput = ((pidoutput + feedback_voltage)/14)+.5;  //this division by 14 needs tried
+		
+		//if(output > 0x14) //temp max duty cycle for debugging
+		//{
+		//	output = 0x14;
+		//}
+		cli();
+		if(pidoutput < 0)
+			OCR1A = 0;
+		else if(!PINC6)
+		{		
+			while(!PINC6 && OCR1A >=0)
+			{
+				OCR1A--;
+			}		
+		}
+		else
+			OCR1A = pidoutput;  //duty cycle
+		sei();
+	
+		
+		previous_error = error;  //Update Previous error
+	}
+	else
+		OCR1A = 0;	
 }
 
-//-----------------------------------------------------------------------
-//This interrupt triggers when the voltage up button is pressed.
+//This interrupt takes an input from a button and increases the set voltage
 //Params: in
-//-----------------------------------------------------------------------
 ISR(INT0_vect){
-	if((setvoltage <= (MAX_VOLTAGE - SET_VOLTAGE_INCREMENT)) && 
-		(switchdebounce >= SWITCH_DEBOUNCE_CYCLES))
+	if((set_voltage <= (maxvoltage - setvoltageincrement)) && (switchdebounce >= 100))  //raise set point voltage if less than the max...
 	{
-		setvoltage += SET_VOLTAGE_INCREMENT;
+		set_voltage += setvoltageincrement;  //update integer variable of set voltage
 		switchdebounce = 0;
 	}
 }
 
-//-----------------------------------------------------------------------
-//This interrupt triggers when the voltage down button is pressed.
+//This interrupt takes an input from a button and decreases the set voltage
 //Params: in
-//-----------------------------------------------------------------------
 ISR(INT1_vect){  
-	if((setvoltage >= SET_VOLTAGE_INCREMENT) && 
-		(switchdebounce >= SWITCH_DEBOUNCE_CYCLES)) 
+	if((set_voltage >= setvoltageincrement) && (switchdebounce >= 100))  //lower set point voltage
 	{
-		setvoltage -= SET_VOLTAGE_INCREMENT;
+		set_voltage -= setvoltageincrement;  //update integer variable of set voltage
 		switchdebounce = 0;
 	}
 }
 
-//-----------------------------------------------------------------------
-//This interrupt triggers when the ADC conversion is finished.  It then
-//stores the ADC value as an integer and restarts the conversion.
+//This interrupt reads the ADC port and converts it to a usable value.
 //Params: in
-//-----------------------------------------------------------------------
 ISR(ADC_vect)
 {
-	tenbitadc = ADCH<<8;  
-	tenbitadc += ADCL;
-	ADCSRA |= 1<<ADSC;  
+	//Assign the variable theLowADC as the value in the register ADCL
+	theLowADC = ADCL; 
+	//Assign theTenBitResults as the value in ADCH shifted 8 left. 
+	theTenBitResults = ADCH<<8;  
+	theTenBitResults += theLowADC;
+	//Start ADC conversion
+	ADCSRA |=1<<ADSC;  
 }
 
-//-----------------------------------------------------------------------
-//This interrupt sets the registers to the correct values for our
-//implementation.
-//Params: none
-void InitializeuC()
-{
-	//Disable Interrupts
-	cli();
-	//Set data direction for ports
-	DDRA	= 0x00;
-	DDRB	= 0xFF;
-	DDRD	= 0xA3;
-	//Set PORTA initial values
-	PORTA	= 0x00;
-	//Set timer options
-	ICR1	= 0x090;
-	OCR1A	= 0x05;
-	TCCR0	= 0x03;
-	TCCR1A	= 0xA2;
-	TCCR1B	= 0x19;
-	//Enable Interrupts
-	ADCSRA	|= 1<<ADIE;
-	GICR	|= 1<<INT0 | 1<<INT1;
-	MCUCR	|= 1<<ISC01 | 1<<ISC00 | 1<<ISC11 | 1<<ISC10;
-	TIMSK	|= 0x01;
-	//Set up ADC
-	ADCSRA	|= 1<<ADPS2 |  1<<ADSC |  1<<ADEN;
-	ADMUX	= 0x40;
-	//Reenable Interrupts
-	sei();
-}
-
-//-----------------------------------------------------------------------
-//This function sends a Character to the specified cursor position on the
-//LCD Display.
+//This function sends a Character to the specified cursor position on 
+//the LCD Display.
 //Params: in, in
-//-----------------------------------------------------------------------
 void LCDChar(char text, unsigned char cursorPosition)
 {
 	LCDCommand(cursorPosition);
@@ -204,10 +214,8 @@ void LCDChar(char text, unsigned char cursorPosition)
 	LCDData(text);
 }
 
-//-----------------------------------------------------------------------
 //This function sends a Register command to the LCD Display.
 //Params: in
-//-----------------------------------------------------------------------
 void LCDCommand(unsigned char command)
 {
 	PORTB = command;
@@ -218,10 +226,8 @@ void LCDCommand(unsigned char command)
 	_delay_ms(1);
 }
 
-//-----------------------------------------------------------------------
 //This function sends a character to the display.
 //Params: in
-//-----------------------------------------------------------------------
 void LCDData(unsigned char data)
 {
 	PORTB = data;
@@ -231,10 +237,8 @@ void LCDData(unsigned char data)
 	PORTD &= ~0x81;
 }
 
-//-----------------------------------------------------------------------
 //This function initializes the LCD Display.
 //Params: none
-//-----------------------------------------------------------------------
 void LCDInit()
 {
 	_delay_ms(32);
@@ -249,11 +253,9 @@ void LCDInit()
 	LCDCommand(0x06);
 }
 
-//-----------------------------------------------------------------------
 //This function sends a string value to the specified cursor position on 
 //the LCD display.
 //Params: ref, in
-//-----------------------------------------------------------------------
 void LCDString(char* text, unsigned char cursorPosition)
 {
 	LCDCommand(cursorPosition);
@@ -264,11 +266,9 @@ void LCDString(char* text, unsigned char cursorPosition)
 	}
 }
 
-//-----------------------------------------------------------------------
 //This function converts a float Voltage to characters and sends them to 
 //the specified cursor position on the LCD Display.
 //Params: in, in
-//-----------------------------------------------------------------------
 void LCDVoltage(int number, unsigned char cursorStartPos)
 {
 	int index;
@@ -281,10 +281,10 @@ void LCDVoltage(int number, unsigned char cursorStartPos)
 		 arr[i-1] = digit;
 		 number = number - pow(10, index) * digit;
 	}
-	char hundredth = arr[3] + CHAR_OFFSET;
-	char tenth = arr[2] + CHAR_OFFSET;
-	char ones = arr[1] + CHAR_OFFSET;
-	char tens = arr[0] + CHAR_OFFSET;
+	char hundredth = arr[3] + charOffset;
+	char tenth = arr[2] + charOffset;
+	char ones = arr[1] + charOffset;
+	char tens = arr[0] + charOffset;
 	
 	LCDChar(tens,cursorStartPos);
 	LCDChar(ones,cursorStartPos + 1);
